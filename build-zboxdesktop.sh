d693ed59d8fe5879b1e5f94f34d3269b8fb98955
Timo Sugliani <timo.sugliani@gmail.com>
2026-01-04 16:07:57 +0000

    Build Debian 13.2 by default

--- build-zboxdesktop.sh
+++ build-zboxdesktop.sh
@@ -4,5 +4,5 @@ rm -rf output-zboxdesktop-*
 
 packer build \
     --var-file="zboxdesktop-builder.json" \
-    --var-file="zboxdesktop-13.1.json" \
+    --var-file="zboxdesktop-13.2.json" \
     zboxdesktop.json
