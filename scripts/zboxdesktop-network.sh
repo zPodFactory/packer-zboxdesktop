8590a6f85b0dddf6f6dbb1b5d145fad3f0e74ea5
Timo Sugliani <timo.sugliani@gmail.com>
2026-01-22 17:18:07 +0000

    Fix OVF properties issue in a vApp, added ttl, install eza binary directly

--- scripts/zboxdesktop-network.sh
+++ scripts/zboxdesktop-network.sh
@@ -23,13 +23,21 @@ apt-get install -y \
   bridge-utils \
   netcat-traditional
 
+
 # Install Doggo fancy DNS Client (json output possible, great with jq)
-curl -sS https://raw.githubusercontent.com/mr-karan/doggo/main/install.sh | /bin/sh && chown root:root /usr/local/bin/doggo
+curl -sS https://raw.githubusercontent.com/mr-karan/doggo/main/install.sh | sh && chown root:root /usr/local/bin/doggo
 
 #
 # Install wakey (wake on lan cli tool)
 # https://github.com/jonathanruiz/wakey
 #
 wget -qO /usr/local/bin/wakey https://github.com/jonathanruiz/wakey/releases/latest/download/wakey_linux_amd64 && chmod +x /usr/local/bin/wakey
+
+#
+# Install ttl (Fast, modern traceroute with real-time TUI)
+# https://github.com/lance0/ttl
+#
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/lance0/ttl/master/install.sh)" <<<'Y' \
+&& chown root:root /usr/local/bin/ttl
 
 echo '> Done'
--- scripts/zboxdesktop-settings.sh
+++ scripts/zboxdesktop-settings.sh
@@ -5,7 +5,7 @@
 ## Misc configuration
 ##
 
-echo '> zBox Settings...'
+echo '> zBoxDesktop Settings...'
 
 echo '> Installing resolvconf...'
 apt-get install -y resolvconf-admin
@@ -15,7 +15,7 @@ echo ""
 echo '> SSH directory'
 mkdir -vp $HOME/.ssh
 
-echo '> zBox acts as a Router now'
+echo '> zBoxDesktop acts as a Router now'
 # Configure via sysctl.d drop-in (Debian may not ship /etc/sysctl.conf by default)
 cat > /etc/sysctl.d/99-zbox.conf << EOF
 net.ipv4.ip_forward = 1
@@ -26,7 +26,7 @@ sysctl --system || true
 
 echo '> Setup Appliance Banner for /etc/issue & /etc/issue.net'
 echo ">>" | tee /etc/issue /etc/issue.net > /dev/null
-echo ">> zBox $(cat /etc/debian_version)" | tee -a /etc/issue /etc/issue.net > /dev/null
+echo ">> zBoxDesktop $(cat /etc/debian_version)" | tee -a /etc/issue /etc/issue.net > /dev/null
 echo ">>" | tee -a /etc/issue /etc/issue.net > /dev/null
 sed -i 's/#Banner none/Banner \/etc\/issue.net/g' /etc/ssh/sshd_config
 
